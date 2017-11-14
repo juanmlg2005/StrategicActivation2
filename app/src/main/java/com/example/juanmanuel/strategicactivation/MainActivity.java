2c04424db22f5611e432a925f6f1522c65975179
Juan Manuel <juanmlg2005@gmail.com>
2017-11-14 14:35:17 -0600

    *Add code to change to other activity acording RaddioBotton

--- app/src/main/java/com/example/juanmanuel/strategicactivation/MainActivity.java
+++ app/src/main/java/com/example/juanmanuel/strategicactivation/MainActivity.java
@@ -1,13 +1,71 @@
 package com.example.juanmanuel.strategicactivation;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RadioGroup radioGroup;
+    private RadioButton radioButton,radioButton1;
+    private Button next;
+    Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Welcome.myWelcome(context);
+        radioButton=(RadioButton)findViewById(R.id.radioButton4);
+        radioButton1=(RadioButton)findViewById(R.id.radioButton5);
+        next=(Button)findViewById(R.id.button);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(radioButton.isChecked()){
+                    Intent intent = new Intent(context, Main2Activity.class);
+                    startActivity(intent);
+                }else if(radioButton1.isChecked()){
+                    Intent intent = new Intent(context, Main2Activity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
     }
 }
