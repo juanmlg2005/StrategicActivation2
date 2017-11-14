907ef94e2c3006a60119fa805b80c35811b125b0
Juan Manuel <juanmlg2005@gmail.com>
2017-11-14 11:43:54 -0600

    *Change colors whit material design *Add life style *Change ic_launcher Icons *Add Welcome java for launch OnCreate Message

---
+++ app/src/main/java/com/example/juanmanuel/strategicactivation/Welcome.java
@@ -0,0 +1,17 @@
+package com.example.juanmanuel.strategicactivation;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Juan Manuel and Rodrigo Ramirez on 14/11/2017.
+ */
+
+public class Welcome {
+
+
+    public static void myWelcome(Context context) {
+        /* Show Message at APK Opend */
+        Toast.makeText(context, "Bienvenido a Strategic Activation",Toast.LENGTH_SHORT).show();
+    }
+}
